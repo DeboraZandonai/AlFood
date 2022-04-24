3f930ff239eb68e284735ef957e02d2f815a2c6e
Debora Luisa Zandonai <60511927+DeboraZandonai@users.noreply.github.com>
2022-04-24 16:34:00 -0300

    FEAT: Adjusted pagination

--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -1,25 +1,43 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import { IPaginacao } from '../../interfaces/IPaginacao';
 import IRestaurante from '../../interfaces/IRestaurante';
 import style from './ListaRestaurantes.module.scss';
 import Restaurante from './Restaurante';
 
 const ListaRestaurantes = () => {
   const [ restaurantes, setRestaurantes ] = useState<IRestaurante[]>([])
+  const [ nextPage, setNextPage ] = useState('');
 
+  
   useEffect(() => {
-    axios.get(`http://localhost:8000/api/v1/restaurantes`)
+    axios.get<IPaginacao<IRestaurante>>(`http://localhost:8000/api/v1/restaurantes`)
       .then(response => {
-       setRestaurantes(response.data.results)
+        setRestaurantes(response.data.results)
+        setNextPage(response.data.next)
       })
       .catch(err => {
         console.log('Houve um erro!', err)
       })
   }, []);
+  
+  const handlePagination = () => {
+    axios.get<IPaginacao<IRestaurante>>(nextPage)
+    .then(response => {
+      setRestaurantes([...restaurantes, ...response.data.results]);
+      setNextPage(response.data.next)
+    })
+    .catch(err => {
+      console.log('Houve um erro!', err)
+    })
+  }
 
   return (<section className={style.ListaRestaurantes}>
     <h1>Os restaurantes mais <em>bacanas</em>!</h1>
     {restaurantes?.map(item => <Restaurante restaurante={item} key={item.id} />)}
+    {nextPage && (
+      <button onClick={handlePagination}>Ver mais</button>
+    )}
   </section>)
 }
 
