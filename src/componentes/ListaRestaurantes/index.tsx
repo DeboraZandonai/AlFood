d5a7e3f69e304cf532b80157cee4c8f85294596c
Debora Luisa Zandonai <60511927+DeboraZandonai@users.noreply.github.com>
2022-04-24 21:55:15 -0300

    FEAT: Alternate pagination

--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -6,39 +6,38 @@ import style from './ListaRestaurantes.module.scss';
 import Restaurante from './Restaurante';
 
 const ListaRestaurantes = () => {
-  const [ restaurantes, setRestaurantes ] = useState<IRestaurante[]>([])
-  const [ nextPage, setNextPage ] = useState('');
 
-  
-  useEffect(() => {
-    axios.get<IPaginacao<IRestaurante>>(`http://localhost:8000/api/v1/restaurantes`)
-      .then(response => {
-        setRestaurantes(response.data.results)
-        setNextPage(response.data.next)
+  const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([])
+  const [proximaPagina, setProximaPagina] = useState('')
+  const [paginaAnterior, setPaginaAnterior] = useState('')
+
+  const handlePagination = (url: string) => {
+    axios.get<IPaginacao<IRestaurante>>(url)
+      .then(resposta => {
+        setRestaurantes(resposta.data.results)
+        setProximaPagina(resposta.data.next)
+        setPaginaAnterior(resposta.data.previous)
       })
-      .catch(err => {
-        console.log('Houve um erro!', err)
+      .catch(erro => {
+        console.log(erro)
       })
-  }, []);
-  
-  const handlePagination = () => {
-    axios.get<IPaginacao<IRestaurante>>(nextPage)
-    .then(response => {
-      setRestaurantes([...restaurantes, ...response.data.results]);
-      setNextPage(response.data.next)
-    })
-    .catch(err => {
-      console.log('Houve um erro!', err)
-    })
   }
+
+  useEffect(() => {
+    handlePagination('http://localhost:8000/api/v1/restaurantes/')
+  }, [])
 
   return (<section className={style.ListaRestaurantes}>
     <h1>Os restaurantes mais <em>bacanas</em>!</h1>
     {restaurantes?.map(item => <Restaurante restaurante={item} key={item.id} />)}
-    {nextPage && (
-      <button onClick={handlePagination}>Ver mais</button>
-    )}
+    {<button onClick={() => handlePagination(paginaAnterior)} disabled={!paginaAnterior}>
+      Página Anterior
+    </button>}
+    {<button onClick={() => handlePagination(proximaPagina)} disabled={!proximaPagina}>
+      Próxima página
+    </button>}
   </section>)
 }
 
-export default ListaRestaurantes
+export default ListaRestaurantes;
+
